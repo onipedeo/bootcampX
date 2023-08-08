71094914f3c72cc3f6abf440b9219df872040074
Tobi Onipede <onipede89@gmail.com>
2023-08-08 05:25:56 +0100

    connect JS apps to the bootcampx database and successfully ran SQL queries that pulled data from the DB

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+console.log(`connected`);
+const args = process.argv.slice(2);
+
+pool.query(`
+  SELECT DISTINCT teachers.name AS teacher,
+  cohorts.name AS cohort
+FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${args[0]}'
+ORDER BY teacher;
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
+    });
+  })
+  .catch(err => {
+    console.log('query error', err.stack);
+  });
