912ac52e678d0394bf3087e3a39fc1ddc5784099
Tobi Onipede <onipede89@gmail.com>
2023-08-08 06:14:14 +0100

    parameterized students.js app

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,13 +9,19 @@ const pool = new Pool({
 
 const args = process.argv.slice(2);
 
-pool.query(`
-  SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+const queryString = `
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
   FROM students
   JOIN cohorts ON cohorts.id = students.cohort_id
-  WHERE cohorts.name LIKE '%${args[0]}%'
-  LIMIT ${args[1]};
-`)
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+const cohortName = args[0];
+const limit = args[1] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
